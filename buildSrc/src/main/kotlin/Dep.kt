d0a2693bf345af4c256347dfad8fb500d2e197e5
zhuzichu520 <zhuzichu520@gmail.com>
2020-03-20 22:27:08 +0800

    update

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -1,7 +1,7 @@
 object Dep {
 
     const val dcendentsGorup = "com.zhuzichu.android"
-    const val dcendentsVersion = "1.0.0"
+    const val dcendentsVersion = "1.0.3"
 
     object Version {
         const val kotlin = "1.3.61"
